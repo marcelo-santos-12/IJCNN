150b958adb75d1b97af4018a89922cc2ddb21d83
marcelosantosd2 <marcelolyond2@gmail.com>
2019-09-10 13:53:03 -0300

    Aperfeiçamento da rotina de teste

--- features_skimage.py
+++ features_skimage.py
@@ -4,39 +4,15 @@ from lbp_module.texture import local_binary_pattern
 import argparse
 
 
-
 def main():
-    ap = argparse.ArgumentParser()
+    name_img = 'DATASET/img_sample/sample_lympho.tif'
+	img = cv2.imread(name_img, 0)
 
-    ap.add_argument("-i", "--input", required=True, \
-            type=str, help="Path to the image")
-    ap.add_argument("-p", "--points", required=False, default=8,\
-	    type=int, help="Number of points surrounding each pixel")
-    ap.add_argument("-r", "--radius", required=False, default=1, \
-	    type=int, help="Radius of circle that surrounding each pixel")
-
-    args = vars(ap.parse_args())
-
-    name_img = args['input']
-    try:
-        img = cv2.imread(name_img, 0)
-
-    except Exception as e:
-        raise Exception(e)
-
-    assert img.ndim == 2, 'Image do not have dimensions equal to 2'
-
-    if args["points"]:
-        P = args["points"]
-
-    if args["radius"]:
-        R = args["radius"]
-
-    feature = local_binary_pattern(img, P=P, R=R)
-
-    cv2.imshow('test', feature)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+	feature_skimage = lbp(img, P=4, R=1)
+	feature_my = local_binary_pattern(img, P=4, R=1)
+	print('Original:\n ', img.shape)
+	print('Module:\n ', feature_my.shape)
+	print('Skimage:\n ', feature_skimage.shape)
 
 if __name__ == '__main__':
 
